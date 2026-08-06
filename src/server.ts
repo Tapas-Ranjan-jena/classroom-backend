05cfead3ad7cfb8b6370301b92456c2b0c85ddd2
Tapas Ranjan Jena <tapasranjan1174@gmail.com>
2026-08-06 15:26:00 +0530

    add extensions

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import subjectRouter from "./routes/subjects.js"
 import cors from "cors";
 import securityMiddleware from "./middleware/security.js";
 import {toNodeHandler} from "better-auth/node"
-import {auth} from "./lib/auth";
+import {auth} from "./lib/auth.js";
 
 const app = express();
 
